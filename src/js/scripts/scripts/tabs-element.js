4fdbaed8c43aa5e2f49c928a99e65575f7d16198
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2025-04-10 04:28:58 +0400

    Рефакторинг логики вкладок в классе TabsElement, упрощение кода и улучшение читаемости. Обновление стилей для класса .wrapper и добавление переменных для управления отступами заголовков. Оптимизация стилей для элементов длины и кнопок, включая улучшение адаптивности и единообразия шрифтов.

--- src/js/scripts/scripts/tabs-element.js
+++ src/js/scripts/scripts/tabs-element.js
@@ -14,24 +14,22 @@ class TabsElement extends HTMLElement {
     this.tabs = this.querySelectorAll('[data-tab]');
     this.panels = this.querySelectorAll('[data-panel]');
 
-    if (this.tabs.length === this.panels.length) {
-      this.abortController = new AbortController();
+    this.abortController = new AbortController();
 
-      this.tabs.forEach((tab) => {
-        const { dataset } = tab;
-        const { tab: tabData } = dataset;
+    this.tabs.forEach((tab) => {
+      const { dataset } = tab;
+      const { tab: tabData } = dataset;
 
-        tab.addEventListener('click', () => {
-          this.tabs.forEach((item) => {
-            item.classList.toggle('active', item.dataset.tab === tabData);
-          });
+      tab.addEventListener('click', () => {
+        this.tabs.forEach((item) => {
+          item.classList.toggle('active', item.dataset.tab === tabData);
+        });
 
-          this.panels.forEach((item) => {
-            item.toggleAttribute('hidden', item.dataset.panel !== tabData);
-          });
-        }, { signal: this.abortController.signal });
-      });
-    }
+        this.panels.forEach((item) => {
+          item.toggleAttribute('hidden', item.dataset.panel !== tabData);
+        });
+      }, { signal: this.abortController.signal });
+    });
   }
 
   disconnectedCallback() {
